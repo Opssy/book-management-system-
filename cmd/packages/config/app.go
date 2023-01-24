bcfbac871830e4a9376063fc68e1bb5dc573b31b
Opssy <opeyemib39@gmail.com>
2023-01-24 02:52:28 +0100

    passing data

--- cmd/packages/config/app.go
+++ cmd/packages/config/app.go
@@ -1,1 +1,13 @@
 package config
+
+import "github.com/jinzhu/gorm"
+
+var {
+	db * gorm.DB
+}
+func Connect(){
+	d, err := gorm.Open("mysql", "ope: ")
+}
+func main() {
+
+}
--- cmd/packages/routes/bookstore-routes.go
+++ cmd/packages/routes/bookstore-routes.go
@@ -1,5 +1,18 @@
 package routes
 
+import (
+	"book-management-system-/cmd/packages/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controllers.Getbook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
+
 func main() {
 
 }
