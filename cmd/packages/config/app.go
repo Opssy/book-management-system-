c25023ef5b7fb08528758447cfb1b3bf11f734d7
Opssy <opeyemib39@gmail.com>
2023-01-25 21:50:20 +0100

    passing data

--- cmd/packages/config/app.go
+++ cmd/packages/config/app.go
@@ -2,11 +2,19 @@ package config
 
 import "github.com/jinzhu/gorm"
 
-var {
-	db * gorm.DB
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "ope:opeyemi123/opetable?charset=uft8&parseTime=True&loc=local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
 }
-func Connect(){
-	d, err := gorm.Open("mysql", "ope: ")
+func GetDB() *gorm.DB {
+	return db
 }
 func main() {
 
--- cmd/packages/modules/book.go
+++ cmd/packages/modules/book.go
@@ -1,1 +1,21 @@
 package modules
+
+import (
+	"book-management-system-/cmd/packages/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm: ""json:"name"`
+	Author      string `gorm: ""json:"author"`
+	Publication string `gorm: ""json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
